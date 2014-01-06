cbeddbaa36d5ebb48de019695a3c68862fb253f6
Magnus Haug <magnus@stack.no>
2014-01-06 10:58:55 +0100

    Lagt til stotte for /messages endepunkt

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
+// if on heroku use heroku port.
+var port = process.env.PORT || 1339;
+var serviceurl = process.env.SERVICE;
+var userpass = process.env.USERPASS;
 
 var demo_url = "https://api.github.com/users/bekkopen/repos";
+
 app.get('/', function(req, res) {
   request.get({
     url: demo_url,
@@ -12,17 +17,34 @@ app.get('/', function(req, res) {
     headers: {
       'User-Agent': 'request'
     }
-  }, function(error, response, body) {
+  },
+  function(error, response, body) {
     if(error) {
-      console.log("an error has occured. keep calm and carry on.");
+      console.log("an error has occured. keep calm and carry on.", error);
     }
     res.json(body);
   });
-
-
 });
 
+app.get('/messages', function(req, res) {
+  request.get({
+    url: serviceurl + '/api/messages',
+    json: true,
+    auth: {
+        user: userpass,
+        pass: userpass,
+        sendImmediately: false
+    },
+    headers: {
+      'User-Agent': 'request'
+    }
+  },
+  function(error, response, body) {
+    if(error) {
+      console.log("an error has occured. keep calm and carry on.", error);
+    }
+    res.json(body);
+  });
+});
 
-// if on heroku use heroku port.
-var port = process.env.PORT || 1339;
 app.listen(port);
