953ba7653ccea94226b4c2f35bc619ad6b743746
Magnus Haug <magnus@stack.no>
2014-01-07 14:23:45 +0100

    Fikset feil i cached-service

--- cachedSocialcastService.js
+++ cachedSocialcastService.js
@@ -3,8 +3,8 @@ var socialcastService = require('./socialcastService');
 var cacheService = require('./mongoService');
 
 exports.getMessage = function(messageid, callback){
-    cacheService.get(messageid, function (err, item) {
-        if (item) {
+    cacheService.get(messageid, function (err, message) {
+        if (message) {
             callback(message);
         } else {
             socialcastService.getMessage(messageid, function(message){
